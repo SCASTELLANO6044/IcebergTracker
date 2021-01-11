4e353bfdc6b71116c3e6a452abdd561ed23f60e3
scast <scast@DESKTOP-FEGREEM>
2021-01-11 11:30:00 +0000

    Version 1

---
+++ src/main/java/icebergTracker/Iceberg.java
@@ -0,0 +1,34 @@
+package icebergTracker;
+
+import java.time.LocalDate;
+
+public class Iceberg {
+    private final int id;
+    private final String origin;
+    private final LocalDate created;
+
+    public Iceberg(int id, String origin, LocalDate created) {
+        this.id = id;
+        this.origin = origin;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    @Override
+    public String toString() {
+        return "Iceberg con id: " + id + ", se origina en: " + origin 
+                + ", en la fecha: " + created;
+    }
+
+}
